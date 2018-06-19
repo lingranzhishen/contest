44b3a8f5d531b10a10ce3f2ff13452f039aee7ff
lingranzhishen <h523588>
2018-06-19 16:22:13 +0800

    lib

--- src/main/java/com/keji/contest/api/AuthenticationApi.java
+++ src/main/java/com/keji/contest/api/AuthenticationApi.java
@@ -51,6 +51,31 @@ public class AuthenticationApi {
         return resultGson;
     }
 
+
+    @PostMapping("authenticationV2")
+    public Object loginV2(@RequestParam(value = "param") String param) {
+            param=URLDecoder.decode(param);
+            LoginReq req = JSON.parseObject(param, LoginReq.class);
+            UserDTO  user=new UserDTO();
+            user.setUsername(req.getUserName());
+            user.setPassword(req.getPassword());
+        UserDTO userInDataBase = userService.findByName(user.getUsername());
+        JSONObject jsonObject = new JSONObject();
+        if (userInDataBase == null) {
+            jsonObject.put("error", "用户不存在");
+        } else if (!userService.comparePassword(user, userInDataBase)) {
+            jsonObject.put("error", "密码不正确");
+        } else {
+            String token = authenticationService.getToken(userInDataBase);
+            jsonObject.put("token", token);
+            userInDataBase.setPassword(null);
+            jsonObject.put("user", userInDataBase);
+        }
+        IntlResultGson resultGson = new IntlResultGson();
+        resultGson.setData(jsonObject);
+        return resultGson;
+    }
+
     @PostMapping("face-authentication")
     public Object faceAuthentication(@RequestParam(value = "param", required = false) String param) {
         FaceLoginReqGson user = JSON.parseObject(param, FaceLoginReqGson.class);
